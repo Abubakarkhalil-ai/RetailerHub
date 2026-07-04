06669bcc3a88ceb097a0b2360b2bd0486301a3ff
Abubakar <Abubakarkhalil55@outlook.com>
2026-07-04 12:56:05 +0500

    Add files via upload
    Testing Files

---
+++ RetailInventoryHub.Tests/ProductServiceTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RetailInventoryHub.Data;
+
+namespace RetailInventoryHub.Tests
+{
+    [TestClass]
+    public class ProductServiceTests
+    {
+        [TestMethod]
+        public void Add_Product_Test()
+        {
+            ProductService service =
+                new ProductService();
+
+            Product p = new Product
+            {
+                ProductId = 1,
+                Barcode = "111",
+                Title = "Milk",
+                Price = 10,
+                QuantityInStock = 20
+            };
+
+            service.Add(p);
+
+            Assert.AreEqual(
+                1,
+                service.GetAll().Count
+            );
+        }
+
+        [TestMethod]
+        public void Delete_Product_Test()
+        {
+            ProductService service =
+                new ProductService();
+
+            Product p = new Product
+            {
+                ProductId = 1,
+                Barcode = "111",
+                Title = "Milk",
+                Price = 10,
+                QuantityInStock = 20
+            };
+
+            service.Add(p);
+
+            service.Delete(1);
+
+            Assert.AreEqual(
+                0,
+                service.GetAll().Count
+            );
+        }
+
+        [TestMethod]
+        public void Linear_Search_Test()
+        {
+            ProductService service =
+                new ProductService();
+
+            Product p = new Product
+            {
+                ProductId = 1,
+                Barcode = "111",
+                Title = "Milk",
+                Price = 10,
+                QuantityInStock = 20
+            };
+
+            service.Add(p);
+
+            ProductLinearSearch search =
+                new ProductLinearSearch();
+
+            Product result =
+                search.Search(
+                    service.GetAll(),
+                    "1");
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
